68c1397b3c6c560bda638f8f97bdd383c26fe731
rosa_mario <rosa_mario@sicredi.com.br>
2021-07-18 12:08:01 -0300

    implementando translation tela login

--- src/components/loginUi/index.tsx
+++ src/components/loginUi/index.tsx
@@ -1,32 +1,25 @@
 import { useState, useContext, useCallback } from 'react';
 import { Button, Input } from 'antd';
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/auth';
+import { useTranslation  } from 'react-i18next';
+import { ParametrosLogin } from '../../interfaces/Usuario';
 
 const LoginUi: React.FC = () => {
-  const history = useHistory();
+
   const [ email, setEmail ] = useState('');
   const [ senha, setSenha ] = useState('');
   const { signIn } = useContext(AuthContext);
-
+  const { t } = useTranslation();
 
   const handleSubmit = useCallback(() => { 
     if( email !== '' && senha !== '' ) {
-      let parametros = {
+      let parametros:ParametrosLogin = {
         email: email.toLowerCase(),
         senha: senha,
       };
       signIn(parametros);
-      // const usuarioApi = new UsuarioApi();
-      // usuarioApi.realizarLogin(email, senha).then( resposta => {
-      //   if(resposta.status === 200) {
-      //     localStorage.setItem( "token-gerenciador-security", resposta.headers.authorization );
-      //   }
-      //   if(localStorage.getItem("token-gerenciador-security")){
-      //     history.push('/')
-      //   }
-      // },(error) => { openNotificationWithIcon('error', 'Dados errados', 'Email ou Senha pode estar incorreto'); });
     }
     
   }, [signIn, email, senha]);
@@ -34,21 +27,21 @@ const LoginUi: React.FC = () => {
   return (
     <>
       <section className="container-form">
-        <h2>Faça seu Login</h2>
+        <h2>{t('login.title')}</h2>
         <div>
-          <input className="btn-login input-login" onBlur={ evt => setEmail( evt.target.value )} placeholder="email@gmail.com" id="email" type="text" name="email" required />
+          <input className="btn-login input-login" onBlur={ evt => setEmail( evt.target.value )} placeholder={t('login.dicaEmail')} id="email" type="text" name="email" required />
           <Input.Password
             required
             className="btn-login"
             id="senha" name="senha"
-            placeholder="insira sua senha"
+            placeholder={t('login.dicaSenha')}
             onBlur={ evt => setSenha( evt.target.value )}
             iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
           />
-          <Button className="btn-cadastrar fundo-azul" onClick={ handleSubmit } >Entrar</Button>
+          <Button className="btn-cadastrar fundo-azul" onClick={ handleSubmit } >{t('login.btEntrar')}</Button>
         </div>
         <Link to='/cadastro' className="btn-cadastrar-usuario">
-            Cadastrar
+          {t('login.btCadastrar')}
         </Link>
       </section>
     </>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,10 +6,11 @@ import  LoginUi  from '../../components/loginUi';
 import './login.css';
 import { Col } from 'antd';
 import logo from '../../img/logo.png';
+import { useTranslation  } from 'react-i18next';
 
 const { Content } = Layout;
-// export default function Login() {
 const Login: React.FC = () => {
+  const { t } = useTranslation();
   const [flg, setFlg ] = useState(true);
   const [ collapsed2, setCollapsed2 ] = useState(false);
   const toggleCollapsed = () => {
@@ -29,11 +30,11 @@ const Login: React.FC = () => {
           <Row>
             <Col xs={{span:24}} md={{span:17}}>
               <div className='container-texto-apresentacao'>
-                <h3>Porque utilizar o gerenciador de exames?</h3>
-                <p>Nosso sistema ajudará você a manter seus exames e consultas organizadas em um só lugar.</p>
-                <p>Nosso sistema tem como objetivo disponibilizar uma alternativa para você organizar seus arquivos de midia digital e fisica de forma     totalmente digital.</p>
-                <p>Disponibilizando acesso de qualquer lugar para você, seja em sua casa, consultório, hospital entre outros lugares.</p>
-                <p className='espaco-botton'>Com nosso sistema você levará sempre todos os seus exames e consultas cadastradas, facilitando o acompanhamento médico e até mesmo ajudando você a ter um controle sobre seus dados de saúde.</p>
+                <h3>{t('telaLogin.title')}</h3>
+                <p>{t('telaLogin.p1')}</p>
+                <p>{t('telaLogin.p2')}</p>
+                <p>{t('telaLogin.p3')}</p>
+                <p className='espaco-botton'>{t('telaLogin.p4')}</p>
               </div>
             </Col>
             <Col xs={{span:24}} md={ { span: 7 } }>
